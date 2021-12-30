238aa080b39791abaa291da9bdd54953039f1468
Farin Lippmann <toucanmeister@gmail.com>
2021-12-30 11:19:39 +0100

    Mean Filter with set radius functional

--- project/main.cpp
+++ project/main.cpp
@@ -46,9 +46,46 @@ void write_image(char filename[], unsigned char *img, int width, int height) {
     img_file.close();
 }
 
-void mean_filter(unsigned char *img, int width, int height, int radius) {
+// Convolutional filter, takes the mean over the 3x3 Square around each pixel
+unsigned char* mean_filter(unsigned char *img, int width, int height) {
     size_t size = width*height*3;
     unsigned char *new_img = new unsigned char[size];
+    for (int i=0; i < height; i++) {
+        for (int j=0; j < width; j++) {
+            for (int c=0; c < 3; c++) {
+                int sum = img[i*width*3 + j*3 + c];
+                if (i-1 >= 0) {
+                    if (j-1 >= 0) {
+                        sum += img[(i-1)*width*3 + (j-1)*3 + c];
+                    }
+                    sum += img[(i-1)*width*3 + j*3 + c];
+                    if (j+1 < width) {
+                        sum += img[(i-1)*width*3 + (j+1)*3 + c];
+                    }
+                }
+                if (j-1 >= 0) {
+                    sum += img[i*width*3 + (j-1)*3 + c];
+                }
+                if (j+1 < width) {
+                    sum += img[i*width*3 + (j+1)*3 + c];
+                }
+                if (i+1 < height) {
+                    if (j-1 >= 0) {
+                        sum += img[(i+1)*width*3 + (j-1)*3 + c];
+                    }
+                    sum += img[(i+1)*width*3 + j*3 + c];
+                    if (j+1 < width) {
+                        sum += img[(i+1)*width*3 + (j+1)*3 + c];
+                    }
+                }
+                new_img[i*width*3 + j*3 + c] =  (char) (sum/9);
+            }
+        }
+    }
+    unsigned char *tmp = img;
+    img = new_img;
+    delete[] tmp;
+    return img;
 }
 
 int main() {
@@ -57,6 +94,7 @@ int main() {
     char out_filename[] = "out.ppm";
     unsigned char *img;
     img = read_image(in_filename, img, &width, &height);
+    img = mean_filter(img, width, height);
     write_image(out_filename, img, width, height);
     delete[] img;
     return 0;
